621bfee4da87e1d04292c0a106ebdf3d8560b9e1
nguyntinkhang <116292903+grimoiredecs@users.noreply.github.com>
2024-04-30 02:05:53 +0700

    First commit

---
+++ init/kDTree.hpp
@@ -0,0 +1,67 @@
+#include "main.hpp"
+#include "Dataset.hpp"
+/* TODO: Please design your data structure carefully so that you can work with the given dataset
+ *       in this assignment. The below structures are just some suggestions.
+ */
+
+// x = 0;   y = 1
+struct kDTreeNode
+{
+    vector<int> data;
+    kDTreeNode *left;
+    kDTreeNode *right;
+    kDTreeNode(vector<int> data, kDTreeNode *left = nullptr, kDTreeNode *right = nullptr)
+    {
+        this->data = data;
+        this->left = left;
+        this->right = right;
+    }
+    bool coord = 0;
+};
+
+class kDTree
+{
+private:
+    int k;
+    kDTreeNode *root;
+
+public:
+    kDTree(int k = 2);
+    ~kDTree();
+    void mergesort(vector<vector<int>> &pointList, int l, int r, int dim);
+    kDTreeNode *findMedian(vector<vector<int>> &pointList, int l, int r, int dim);
+    const kDTree &operator=(const kDTree &other);
+    kDTree(const kDTree &other);
+
+    void inorderTraversal() const;
+    void preorderTraversal() const;
+    void postorderTraversal() const;
+    int height() const;
+    int nodeCount() const;
+    int leafCount() const;
+
+    void insert(const vector<int> &point);
+    void remove(const vector<int> &point);
+    bool search(const vector<int> &point);
+    void buildTree(const vector<vector<int>> &pointList);
+    void nearestNeighbour(const vector<int> &target, kDTreeNode *best);
+    void kNearestNeighbour(const vector<int> &target, int k, vector<kDTreeNode *> &bestList);
+};
+
+class kNN
+{
+private:
+    int k;
+    Dataset *X_train;
+    Dataset *y_train;
+    int numClasses;
+    kDTree *tree;
+
+public:
+    kNN(int k = 5);
+    void fit(Dataset &X_train, Dataset &y_train);
+    Dataset predict(Dataset &X_test);
+    double score(const Dataset &y_test, const Dataset &y_pred);
+};
+
+// Please add more or modify as needed
